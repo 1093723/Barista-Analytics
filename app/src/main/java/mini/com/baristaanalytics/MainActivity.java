781a683584cf6aacecd592525230dbc6cef5b42a
Tony Lancer <tony12lancer@gmail.com>
2018-08-13 13:29:29 +0200

    Update Firebase database dependencies

--- app/src/main/java/mini/com/baristaanalytics/MainActivity.java
+++ app/src/main/java/mini/com/baristaanalytics/MainActivity.java
@@ -22,4 +22,8 @@ public class MainActivity extends AppCompatActivity {
         Intent x = new Intent(this, LoginActivity.class );
         startActivity(x);
     }
+    public void registerVendor(View view){
+        Intent x = new Intent(this, RegisterVendorActivity.class);
+        startActivity(x);
+    }
 }
