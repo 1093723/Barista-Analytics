64bbffd37d964f89e83f5751345641dd47b0f195
sontaga <sonti96@live.co.uk>
2018-08-21 09:18:33 +0200

    Google Maps UI allows user to see their device location

--- app/src/main/java/mini/com/baristaanalytics/MapsActivity.java
+++ app/src/main/java/mini/com/baristaanalytics/MapsActivity.java
@@ -2,6 +2,7 @@ package mini.com.baristaanalytics;
 
 import android.app.Dialog;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -13,12 +14,19 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
     public static final int ERROR_DIALOG_REQUEST = 9001;
+    private static final float DEFAULT_ZOOM = 15f;
     public String TAG = "SEARCH COFFEE PLACES: ACTIVITY";
     public static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     public static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
@@ -27,12 +35,23 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     // vars
     private Boolean mPermissionGranted = false;
     private GoogleMap gMap;
+    private FusedLocationProviderClient mproviderClient;
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         Toast.makeText(this, "Map is ready", Toast.LENGTH_SHORT).show();
         Log.d(TAG, "onMapReady : Map is ready");
         gMap = googleMap;
+        if (mPermissionGranted) {
+            getDeviceLocation();
+            if (ActivityCompat.checkSelfPermission(this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            gMap.setMyLocationEnabled(true);
+        }
     }
 
     @Override
@@ -44,6 +63,49 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             get_permission_location();
         }
     }
+    /**
+     * This method is for getting the device location
+     */
+    private void getDeviceLocation(){
+        Log.d(TAG, "getDeviceLocation(): getting the current devices' location");
+        mproviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        try{
+            if(mPermissionGranted){
+                Task location = mproviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "getDeviceLocation(): Location found");
+                            Location currentLocation = (Location) task.getResult();
+                            moveCamera(new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude()),
+                                    DEFAULT_ZOOM);
+                        }else {
+                            Log.d(TAG, "getDeviceLocation(): Location not found");
+                            Toast.makeText(MapsActivity.this,
+                                    "Unable to get current location", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+
+        }catch (SecurityException e){
+            Log.d(TAG, "getDeviceLocation(): Security Exception: " + e.getMessage());
+
+        }
+    }
+
+    /**
+     * This method recenters the position of the zoom to the devices' currrent location on the map
+     * @param latLng (contains the longitude and latitude values)
+     * @param zoom
+     */
+    private void moveCamera(LatLng latLng, float zoom){
+        Log.d(TAG, "moveCamera(); moving camera to lat: " + latLng.latitude +
+                ". Longitude: " + latLng.longitude);
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoom));
+    }
 
     /**
      * This method is to initialize the user interface of the Maps API
@@ -54,6 +116,13 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapFragment.getMapAsync(this);
     }
 
+    /**
+     * This method is for getting user permissions for the device location
+     * It keeps a record of the 2 permissions pertaining to a location and then
+     * it checks if we have the permission in the current context
+     *
+     * It will initialize the map if we have permissions and not, otherwise
+     */
     public void get_permission_location(){
         Log.d(TAG, "get_permission_location(): Getting location permission");
         String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
@@ -69,6 +138,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             }else {
                 ActivityCompat.requestPermissions(this, permissions,LOCATION_PERMISSION_REQUEST_CODE);
             }
+        }else {
+            ActivityCompat.requestPermissions(this, permissions,LOCATION_PERMISSION_REQUEST_CODE);
         }
     }
 
