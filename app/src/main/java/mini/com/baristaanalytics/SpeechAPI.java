d4c26e07154f4f4a919ba132e42a704f40abe196
sontaga <sonti96@live.co.uk>
2018-09-03 17:37:02 +0200

    Added AWS Polly on startup for text to speech

--- app/src/main/java/mini/com/baristaanalytics/SpeechAPI.java
+++ app/src/main/java/mini/com/baristaanalytics/SpeechAPI.java
@@ -3,6 +3,9 @@ package mini.com.baristaanalytics;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
@@ -12,10 +15,23 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.polly.AmazonPollyPresigningClient;
+import com.amazonaws.services.polly.model.DescribeVoicesRequest;
+import com.amazonaws.services.polly.model.DescribeVoicesResult;
+import com.amazonaws.services.polly.model.OutputFormat;
+import com.amazonaws.services.polly.model.SynthesizeSpeechPresignRequest;
+import com.amazonaws.services.polly.model.Voice;
+
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -37,6 +53,25 @@ public class SpeechAPI extends AppCompatActivity{
     private final int REQ_CODE_SPEECH_INPUT = 100;
     private TextToSpeech textToSpeech;
 
+    //AWS Polly Vars
+    CognitoCachingCredentialsProvider credentialsProvider;
+    private List<Voice> voices;
+
+    // Amazon Polly permissions.
+    private static final String COGNITO_POOL_ID = "CHANGEME";
+
+    // Region of Amazon Polly.
+    private static final Regions MY_REGION = Regions.US_EAST_1;
+    private AmazonPollyPresigningClient client;
+
+    // AWS Media Player
+    MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        setupVoicesList();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,9 +79,9 @@ public class SpeechAPI extends AppCompatActivity{
         ctx = this;
         message = (TextView) findViewById(R.id.message);
         btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
-        //message_items = new ArrayList<>();
-        // hide the action bar
-        //getActionBar().hide();
+        initPollyClient();
+        setupNewMediaPlayer();
+
         textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
@@ -62,12 +97,96 @@ public class SpeechAPI extends AppCompatActivity{
                 promptSpeechInput();
             }
         });
+    }
+    /**
+     * Setup responding to the user
+     */
+    private void setupPlayButton(String words){
+        if(words.equals("welcome")){
+            String welcome_message = "Hello. I'm Bruce. Your voice-assistant Barista throughout your " +
+                    "use of the application. I can help you search for coffee places and view available coffee shops near you";
+            message_Item item = new message_Item(welcome_message);
+            message_items.add(item);
+            // Create speech synthesis request.
+            SynthesizeSpeechPresignRequest synthesizeSpeechPresignRequest =
+                    new SynthesizeSpeechPresignRequest()
+                            // Set text to synthesize.
+                            .withText(welcome_message)
+                            // Set voice selected by the user.
+                            .withVoiceId(voices.get(33).getId())
+                            // Set format to MP3.
+                            .withOutputFormat(OutputFormat.Mp3);
 
+            // Get the presigned URL for synthesized speech audio stream.
+            URL presignedSynthesizeSpeechUrl =
+                    client.getPresignedSynthesizeSpeechUrl(synthesizeSpeechPresignRequest);
 
+            Log.i(TAG, "Playing speech from presigned URL: " + presignedSynthesizeSpeechUrl);
 
+            // Create a media player to play the synthesized audio stream.
+            if (mediaPlayer.isPlaying()) {
+                setupNewMediaPlayer();
+            }
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+            try {
+                // Set media player's data source to previously obtained URL.
+                mediaPlayer.setDataSource(presignedSynthesizeSpeechUrl.toString());
+            } catch (IOException e) {
+                Log.e(TAG, "Unable to set data source for the media player! " + e.getMessage());
+            }
+
+            // Start the playback asynchronously (since the data source is a network stream).
+            mediaPlayer.prepareAsync();
+        }else {
+            // Create speech synthesis request.
+            SynthesizeSpeechPresignRequest synthesizeSpeechPresignRequest =
+                    new SynthesizeSpeechPresignRequest()
+                            // Set text to synthesize.
+                            .withText(words)
+                            // Set voice selected by the user.
+                            .withVoiceId(voices.get(32).getId())
+                            // Set format to MP3.
+                            .withOutputFormat(OutputFormat.Mp3);
+
+            // Get the presigned URL for synthesized speech audio stream.
+            URL presignedSynthesizeSpeechUrl =
+                    client.getPresignedSynthesizeSpeechUrl(synthesizeSpeechPresignRequest);
+
+            Log.i(TAG, "Playing speech from presigned URL: " + presignedSynthesizeSpeechUrl);
+
+            // Create a media player to play the synthesized audio stream.
+            if (mediaPlayer.isPlaying()) {
+                setupNewMediaPlayer();
+            }
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+            try {
+                // Set media player's data source to previously obtained URL.
+                mediaPlayer.setDataSource(presignedSynthesizeSpeechUrl.toString());
+            } catch (IOException e) {
+                Log.e(TAG, "Unable to set data source for the media player! " + e.getMessage());
+            }
+
+            // Start the playback asynchronously (since the data source is a network stream).
+            mediaPlayer.prepareAsync();
+        }
 
     }
+    /**
+     * Initialize amazon polly
+     */
+    private void initPollyClient() {
+        // Initialize the Amazon Cognito credentials provider.
+        credentialsProvider = new CognitoCachingCredentialsProvider(
+                getApplicationContext(),
+                "us-east-1:7e3cee5d-a9dc-4951-8d7c-63275541e8e3", // Identity pool ID
+                Regions.US_EAST_1 // Region
+        );
 
+        // Create a client that supports generation of presigned URLs.
+        client = new AmazonPollyPresigningClient(credentialsProvider);
+    }
     /**
      * Showing google speech input dialog
      * */
@@ -88,14 +207,49 @@ public class SpeechAPI extends AppCompatActivity{
         }
 
     }
+    /**
+     * Setup Polly Voices List
+     */
+    void setupVoicesList() {
+        // Asynchronously get available Polly voices.
+        new GetPollyVoices().execute();
 
+    }
+    /**
+     * AWS Polly Media Player
+     */
+
+
+    void setupNewMediaPlayer() {
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.release();
+                setupNewMediaPlayer();
+            }
+        });
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.start();
+                //playButton.setEnabled(true);
+            }
+        });
+        mediaPlayer.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+            @Override
+            public boolean onError(MediaPlayer mp, int what, int extra) {
+                //playButton.setEnabled(true);
+                return false;
+            }
+        });
+    }
     /**
      * Receiving speech input
      * */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         switch (requestCode) {
             case REQ_CODE_SPEECH_INPUT: {
                 if (resultCode == RESULT_OK && null != data) {
@@ -110,7 +264,11 @@ public class SpeechAPI extends AppCompatActivity{
                     if(result.get(0).contains("like") && result.get(0).contains("coffee")){
                         Intent x = new Intent(this, MapsActivity.class);
                         String toSpeak = "Proceeding to user registration";
-                        textToSpeech.speak(toSpeak, TextToSpeech.QUEUE_FLUSH,null);
+                        message_Item message = new message_Item(toSpeak);
+                        message_items.add(message);
+                        //Trigger AWS Polly
+                        setupPlayButton("We're Almost There");
+                        //textToSpeech.speak(toSpeak, TextToSpeech.QUEUE_FLUSH,null);
                         startActivity(x);
                         finish();
                     }
@@ -149,5 +307,37 @@ public class SpeechAPI extends AppCompatActivity{
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new MyAdapter(message_items,this);
         recyclerView.setAdapter(adapter);
+    }
+
+    private class GetPollyVoices extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            if (voices != null) {
+                return null;
+            }
+
+            // Create describe voices request.
+            DescribeVoicesRequest describeVoicesRequest = new DescribeVoicesRequest();
+
+            DescribeVoicesResult describeVoicesResult;
+            try {
+                // Synchronously ask the Polly Service to describe available TTS voices.
+                describeVoicesResult = client.describeVoices(describeVoicesRequest);
+            } catch (RuntimeException e) {
+                Log.e(TAG, "Unable to get available voices. " + e.getMessage());
+                return null;
+            }
+
+            // Get list of voices from the result.
+            voices = describeVoicesResult.getVoices();
+
+            // Log a message with a list of available TTS voices.
+            Log.i(TAG, "Available Polly voices: " + voices);
+
+            setupPlayButton("welcome");
+            return null;
+        }
+
+
     }
 }
