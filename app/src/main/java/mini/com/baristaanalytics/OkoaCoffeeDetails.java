175c3383bfdceefa237bc648dd3a5591e8b8d8ca
Omni-Script <1144501@students.wits.ac.za>
2018-10-11 21:23:02 +0200

    Layout Issue updated

---
+++ app/src/main/java/mini/com/baristaanalytics/OkoaCoffeeDetails.java
@@ -0,0 +1,13 @@
+package mini.com.baristaanalytics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OkoaCoffeeDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_okoa_coffee_details);
+    }
+}
