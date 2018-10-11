4ca442512a5755aea1474112e5c6a35a8871a8d2
Omni-Script <1144501@students.wits.ac.za>
2018-10-11 21:29:11 +0200

    Layout Issue updated

--- app/src/main/java/mini/com/baristaanalytics/Okoa/OkoaCoffeeDetails.java
+++ app/src/main/java/mini/com/baristaanalytics/Okoa/OkoaCoffeeDetails.java
@@ -1,7 +1,9 @@
-package mini.com.baristaanalytics;
+package mini.com.baristaanalytics.Okoa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import mini.com.baristaanalytics.R;
 
 public class OkoaCoffeeDetails extends AppCompatActivity {
 
