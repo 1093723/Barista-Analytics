c03ed3c9a0fb1d4c3847d6dd4373d1073c3c31d5
Lerato <leratomakopoi@gmail.com>
2018-10-11 20:45:24 +0200

    Order Class is ready

---
+++ app/src/main/java/Services/OrderService.java
@@ -0,0 +1,40 @@
+package Services;
+
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.Calendar;
+
+import Model.CoffeeOrder;
+
+public class OrderService {
+    public String OrderStore;
+    public String userName;
+    public String OrderDate;
+    public int Order_Total;
+    public String OrderDescription;
+
+    public OrderService() {
+        OrderDate = java.text.DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime());
+    }
+
+    public int getOrder_Total() {
+        return Order_Total;
+    }
+
+    public Boolean processOrder(String OrderStore, String userName, String quantity, String nameOfCoffee, int beverage_price, DatabaseReference databaseOrder){
+        Boolean flag = false;
+        try {
+            String id = databaseOrder.push().getKey();
+            int int_id = Integer.parseInt(id);
+            this.OrderDescription = quantity + " x " + nameOfCoffee + " on " + OrderDate;
+            CoffeeOrder order = new CoffeeOrder(int_id, OrderDescription, OrderStore, userName, beverage_price);
+            if(databaseOrder.child("Order").child(id).setValue(order).isSuccessful()){
+                flag = true;
+            }
+        }catch (NullPointerException e){
+            flag = true;
+        }
+        return flag;
+
+    }
+}
