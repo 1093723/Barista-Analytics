faa86d9c96e12ac47717a582c1006db2a4cab391
sontaga <sonti96@live.co.uk>
2018-10-25 09:26:45 +0200

    Attempt to fix build

--- app/src/main/java/utilities/SMS.java
+++ app/src/main/java/utilities/SMS.java
@@ -1,5 +1,5 @@
 package utilities;
-import com.nexmo.client.NexmoClient;
+/*import com.nexmo.client.NexmoClient;
 import com.nexmo.client.NexmoClientException;
 import com.nexmo.client.auth.AuthMethod;
 import com.nexmo.client.auth.TokenAuthMethod;
@@ -7,10 +7,10 @@ import com.nexmo.client.sms.SmsSubmissionResult;
 import com.nexmo.client.sms.messages.TextMessage;
 
 import java.io.IOException;
-
+*/
 public class SMS {
     public void sendSMS(){
-        AuthMethod auth = new TokenAuthMethod("ff1ccc60", "guMrsm45B7BlGOQw");
+        /*AuthMethod auth = new TokenAuthMethod("ff1ccc60", "guMrsm45B7BlGOQw");
         NexmoClient client = new NexmoClient(auth);
         TextMessage message = new TextMessage("12622000030", "0843968418", "Hello from Nexmo!");
         SmsSubmissionResult[] responses = new SmsSubmissionResult[0];
@@ -24,5 +24,5 @@ public class SMS {
         for (SmsSubmissionResult response : responses) {
             System.out.println(response);
         }
-    }
+    */}
 }
