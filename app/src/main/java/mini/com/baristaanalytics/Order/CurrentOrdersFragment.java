5d50f74a5c0829e0a8efa425a7eb33a9ca944138
Tony Lancer <tony12lancer@gmail.com>
2018-11-05 18:41:48 +0200

    Finished with Current Admin Order View.

--- app/src/main/java/mini/com/baristaanalytics/Order/CurrentOrdersFragment.java
+++ app/src/main/java/mini/com/baristaanalytics/Order/CurrentOrdersFragment.java
@@ -19,7 +19,7 @@ public class CurrentOrdersFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_order_current,container,false);
+        View view = inflater.inflate(R.layout.fragment_admin_current_order,container,false);
         database = FirebaseDatabase.getInstance();
         current_orders = database.getReference("CoffeeMenuOkoa");
         return view;
