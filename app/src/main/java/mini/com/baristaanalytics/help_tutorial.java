0a953a82614d80b033141e83f14f3a959f7c4e36
Omni-Script <1144501@students.wits.ac.za>
2018-11-08 17:20:47 +0200

    fix

---
+++ app/src/main/java/mini/com/baristaanalytics/help_tutorial.java
@@ -0,0 +1,13 @@
+package mini.com.baristaanalytics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class help_tutorial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_tutorial);
+    }
+}
