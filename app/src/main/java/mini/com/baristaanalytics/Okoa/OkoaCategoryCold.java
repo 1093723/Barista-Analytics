62f38ee98e65fdb8f38d9914d41043cfb665e56b
sontaga <sonti96@live.co.uk>
2018-11-10 09:57:47 +0200

    Refactoring OkoaCategoryCold

--- app/src/main/java/mini/com/baristaanalytics/Okoa/OkoaCategoryCold.java
+++ app/src/main/java/mini/com/baristaanalytics/Okoa/OkoaCategoryCold.java
@@ -165,35 +165,13 @@ public class OkoaCategoryCold extends AppCompatActivity {
                 viewPager = findViewById(R.id.viewPager);
                 viewPager.setAdapter(adapter);
                 viewPager.setPadding(130,0,130,0);
-                Integer[] colors_temp = {
-                        getResources().getColor(R.color.color30),
-                        getResources().getColor(R.color.color2),
-                        getResources().getColor(R.color.color3),
-                        getResources().getColor(R.color.color5),
-                        getResources().getColor(R.color.color30),
-                        getResources().getColor(R.color.color2),
-                        getResources().getColor(R.color.color3),
-                        getResources().getColor(R.color.color5),
-                        getResources().getColor(R.color.color30),
-                        getResources().getColor(R.color.color2),
-                        getResources().getColor(R.color.color3),
-                        getResources().getColor(R.color.color5),
-
-                };
-
-                colors = colors_temp;
                 viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
                     @Override
                     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                        if(position < (adapter.getCount() - 1) && position < (colors.length - 1)){
                             beverage = models.get(position);
                             txtViewPriceLarge.setText(beverage.getPrice_tall().toString());
                             txtViewPriceSmall.setText(beverage.getPrice_small().toString());
                             viewPager.setBackgroundColor((Integer) argbEvaluator.evaluate(positionOffset, colors[position], colors[position + 1]));
-                        }
-                        else {
-                            viewPager.setBackgroundColor(colors[colors.length - 1]);
-                        }
                     }
 
                     @Override
